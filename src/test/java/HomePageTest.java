cc7e1dd66d6f649924f0f53cb454e5bdc7cd64d2
Khalandr <48185617+Khalandr@users.noreply.github.com>
2022-08-28 12:15:04 +0200

    test 2: czy są "empty names" w "popular"

--- src/test/java/HomePageTest.java
+++ src/test/java/HomePageTest.java
@@ -1,14 +1,18 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class HomePageTest {
 
     private WebDriver driver; // komunikacja z przeglądarką
-    private WebElement element; // obsługa elenentów na stronie
+    private WebElement elements; // obsługa elenentów na stronie
 
     // uruchomi się przed wszystkimi testami
     @BeforeAll
@@ -34,6 +38,17 @@ public class HomePageTest {
         driver.get("http://automationpractice.com/index.php");
         Assertions.assertThat(driver.getTitle()).isEqualTo("My Store");
         System.out.println(driver.getTitle());
+    }
+
+    @Test
+    public void shouldSeePopularItemsOnHomePage() {
+        driver.get("http://automationpractice.com/index.php");
+        List<WebElement> productNames = driver.findElements(By.cssSelector("#homefeatured .product-name"));
+
+        List<WebElement> productsWithEmpyName = productNames.stream()
+                .filter(productName -> productName.getText().isEmpty())
+                .collect(Collectors.toList());
+        Assertions.assertThat(productsWithEmpyName).isEmpty();
     }
 
 
